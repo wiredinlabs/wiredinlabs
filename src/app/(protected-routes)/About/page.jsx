ea28e64b5784e479230a1be2a914056719470674
MJ10X <24100188@lums.edu.pk>
2025-02-09 20:24:24 +0500

    dev branch init with project structure

---
+++ src/app/(protected-routes)/About/page.jsx
@@ -0,0 +1,14 @@
+// Temporary Page for Developing About Section
+// Just add the reference of About Component in this so that /About can be used to see the About Section
+// Later on add the About Component in the main page
+
+import AboutSection from "@/components/home_components/About/AboutSection";
+
+export default function About() {
+  return (
+    <div className="">
+     About Page
+     <AboutSection />
+    </div>
+  );
+}
---
+++ src/components/home_components/About/AboutSection.jsx
@@ -0,0 +1,9 @@
+// Main About Section Component
+export default function AboutSection() {
+    return (
+      <div className="">
+       About Section here
+      </div>
+    );
+  }
+  
