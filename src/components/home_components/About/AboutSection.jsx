8026c75107246900fe9e40500bf8f58091d214f7
Asjal Rashidi <88508322+asjalrashidi01@users.noreply.github.com>
2025-02-09 20:45:07 +0500

    layout init with metadata and fonts

--- src/components/home_components/About/AboutSection.jsx
+++ src/components/home_components/About/AboutSection.jsx
@@ -1,8 +1,8 @@
 // Main About Section Component
 export default function AboutSection() {
     return (
-      <div className="">
-       About Section here
+      <div>
+        About Section
       </div>
     );
   }
