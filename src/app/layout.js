95f502dcf3a9a226b3d6a8dd4103c20f812541b1
Asjal Rashidi <88508322+asjalrashidi01@users.noreply.github.com>
2025-02-09 23:43:10 +0500

    added all content minus star

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 
