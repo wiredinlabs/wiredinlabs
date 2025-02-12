de9a04832adcd36f2226ea76950ed0e24f636715
MJ10X <24100188@lums.edu.pk>
2025-02-12 23:51:43 +0500

    navbar fixes

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 import About from "./(protected-routes)/About/page";
 import AboutSection from "@/components/home_components/About/AboutSection";
 import ServicesSection from "@/components/home_components/Services/ServicesSection";
+import WorksSection from "@/components/home_components/Works/WorksSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full min-h-screen font-[family-name:var(--font-geist-sans)] mt-[10vh]">
     <AboutSection />
     <ServicesSection />
+    <WorksSection />
+
     </div>
   );
 }
--- src/components/layout_components/NavBar/NavBar.jsx
+++ src/components/layout_components/NavBar/NavBar.jsx
@@ -49,7 +49,7 @@ const NavBar = () => {
 
   return (
     <div
-      className={`text-black h-[17vh] w-full flex justify-between bg-black items-center px-20 py-1 fixed top-0 shadow-md z-50 transition-transform duration-500 ${
+      className={`text-black h-[17vh] w-full flex justify-between bg-black items-center px-20 py-1 fixed top-0 shadow-md z-50 transition-transform duration-1000 ${
         isVisible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
@@ -89,10 +89,10 @@ const NavBar = () => {
 
       {/* Contact Button */}
       <div
-        className="flex justify-center items-center text-black bg-[#E4ED05] border-2 border-black rounded-3xl px-4 py-2 text-xl font-medium cursor-pointer 
-    hover:bg-black hover:border-[#E4ED05] hover:text-white transition-all duration-500"
+        className="flex justify-center items-center text-black bg-[#E4ED05] border-2  rounded-3xl px-4 py-1 text-xl font-medium cursor-pointer 
+    hover:bg-black border-[#E4ED05] hover:text-white transition-all duration-500 shadow-[0_0_10px_rgba(0,0,0,0.5)] shadow-[#E4ED05]"
       >
-        Contact
+        Contact Us
       </div>
     </div>
   );
