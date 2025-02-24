929d57e24a7c5152b2628f63826e89e844f92802
MJ10X <24100188@lums.edu.pk>
2025-02-25 03:38:17 +0500

    blogs done

--- src/components/home_components/Blogs/BlogsSection.jsx
+++ src/components/home_components/Blogs/BlogsSection.jsx
@@ -4,13 +4,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/scrollbar";
 import { useRouter } from "next/navigation";
+import "./Blogs.css";
 
 const blogs = [
   {
     id: 1,
     title: "Random Shit 1",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/1",
   },
@@ -18,7 +19,7 @@ const blogs = [
     id: 2,
     title: "Random Shit 2",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/2",
   },
@@ -26,7 +27,7 @@ const blogs = [
     id: 3,
     title: "Random Shit 3",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/3",
   },
@@ -34,7 +35,7 @@ const blogs = [
     id: 4,
     title: "Random Shit 4",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/4",
   },
@@ -42,7 +43,7 @@ const blogs = [
     id: 5,
     title: "Random Shit 5",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/5",
   },
@@ -50,7 +51,7 @@ const blogs = [
     id: 6,
     title: "Random Shit 6",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
     image: "/images/blog_image.png",
     link: "/blog/6",
   },
@@ -60,51 +61,47 @@ const BlogsSection = () => {
   const router = useRouter();
 
   return (
-    <div
-      id="blogs"
-      className="min-h-screen bg-black text-white flex flex-col items-center pt-10 relative w-full"
-    >
-      <h2 className="text-6xl font-bold mb-8 text-left w-full px-8">
+    <div id="blogs" className="min-h-screen pt-[15vh] bg-black text-white flex flex-col items-center relative w-full">
+      {/* Title */}
+      <h2 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-6 sm:mb-8 w-full px-5 sm:px-8">
         Blogs<span className="text-[#E4ED05]">.</span>
       </h2>
 
-      <div className="w-full px-5 relative">
+      <div className="w-full px-3 sm:px-5 relative">
         <Swiper
-          slidesPerView={3.5}
-          spaceBetween={20}
+          slidesPerView={1.2} // Default: mobile-first approach
+          spaceBetween={15}
           grabCursor={true}
-          cssMode={true} // Enables natural scrolling with trackpad or touch
+          cssMode={true}
           touchEventsTarget="container"
-          scrollbar={{ draggable: true }} // Enables scrolling with two fingers
+          scrollbar={{ draggable: true }}
+          breakpoints={{
+            640: { slidesPerView: 1.5, spaceBetween: 15 }, // Small screens
+            768: { slidesPerView: 2.5, spaceBetween: 20 }, // Tablets
+            1024: { slidesPerView: 3.5, spaceBetween: 20 }, // Laptops
+            1280: { slidesPerView: 4.5, spaceBetween: 25 }, // Desktops
+          }}
           className="relative"
         >
           {blogs.map((blog) => (
             <SwiperSlide key={blog.id} className="overflow-visible">
               <div
                 onClick={() => router.push(blog.link)}
-                className="cursor-pointer mt-2 bg-white text-black shadow-lg transition-transform transform hover:scale-105 h-[600px] flex-shrink-0 relative overflow-visible"
+                className="cursor-pointer mt-2 bg-white text-black shadow-lg transition-transform transform hover:scale-105 h-[450px] sm:h-[500px] flex-shrink-0 relative overflow-hidden"
               >
                 <div className="p-2 bg-white">
-                  <img
-                    src={blog.image}
-                    alt={blog.title}
-                    className="w-full h-[40%] object-cover"
-                  />
+                  <img src={blog.image} alt={blog.title} className="w-full h-[40%] object-cover" />
                 </div>
                 <div className="p-4 flex flex-col h-[60%]">
-                  <h3 className="text-4xl font-bold h-[10%] flex items-center tracking-tighter mb-2">
+                  <h3 className="text-2xl sm:text-3xl font-bold h-[15%] flex items-center tracking-tighter mb-2">
                     {blog.title}
                   </h3>
-                  <p className="text-md opacity-70 h-[90%] overflow-hidden tracking-tighter italic">
+                  <p className="text-sm opacity-70 h-[85%] overflow-hidden tracking-tighter italic">
                     {blog.description}
                   </p>
                 </div>
-                <div className="absolute bottom-4 right-4 w-8 h-8">
-                  <img
-                    src="/images/small_image.png"
-                    alt="small icon"
-                    className="w-full object-cover"
-                  />
+                <div className="absolute bottom-4 right-4 w-6 h-6 sm:w-8 sm:h-8">
+                  <img src="/images/small_image.png" alt="small icon" className="w-full object-cover" />
                 </div>
               </div>
             </SwiperSlide>
