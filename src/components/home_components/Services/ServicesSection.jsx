709dfacc8a97c8f1b976d90634d734ad84628d9a
MJ10X <24100188@lums.edu.pk>
2025-04-29 23:25:17 +0500

    added the three lines

--- src/components/home_components/Services/ServicesSection.jsx
+++ src/components/home_components/Services/ServicesSection.jsx
@@ -36,7 +36,9 @@ const ServicesSection = () => {
 
       if (newService !== activeService) {
         setActiveService(newService);
-        setRotation((prev) => prev + (scrollDirection === "down" ? 70.7 : -70.7));
+        setRotation(
+          (prev) => prev + (scrollDirection === "down" ? 70.7 : -70.7)
+        );
       }
 
       setLastScrollY(window.scrollY); // Update last scroll position
@@ -78,7 +80,11 @@ const ServicesSection = () => {
             ${isFullyVisible ? "min-h-screen" : "min-h-[90vh] mt-12"}
             bg-[#E4ED05] text-black relative
             transition-all duration-300 ease-in-out
-            ${isFullyVisible ? "mx-0 rounded-none" : "mx-4 md:mx-8 lg:mx-14 rounded-3xl"}
+            ${
+              isFullyVisible
+                ? "mx-0 rounded-none"
+                : "mx-4 md:mx-8 lg:mx-14 rounded-3xl"
+            }
             flex flex-col justify-center
           `}
         >
@@ -87,8 +93,8 @@ const ServicesSection = () => {
             className="absolute top-4 md:top-6 lg:top-8 left-4 md:left-6 lg:left-8 transition-transform duration-300 ease-in-out"
             style={{ transform: `rotate(${rotation}deg)` }}
           >
-            <svg 
-              viewBox="0 0 177 164" 
+            <svg
+              viewBox="0 0 177 164"
               className="w-20 h-20 md:w-24 md:h-24 lg:w-44 lg:h-44 fill-current"
             >
               <path
@@ -109,12 +115,44 @@ const ServicesSection = () => {
                     leading-none font-bold transition-all duration-300 relative
                     ${
                       activeService === service
-                        ? "text-black text-[38px] sm:text-[70px] md:text-[85px] lg:text-[100px] font-bold tracking-tighter opacity-100"
+                        ? "text-black text-[38px] sm:text-[70px] md:text-[85px] lg:text-[110px] font-bold tracking-tighter opacity-100"
                         : "text-black/30 text-[30px] sm:text-[40px] md:text-[60px] lg:text-[70px] font-bold tracking-tighter opacity-70"
                     }
                   `}
                 >
                   {service}
+                  <div className={`absolute ${
+                      activeService === service
+                        ? "block"
+                        : "hidden"
+                    }`} style={{ top: "-37px", left: "-44px" }}>
+                    <svg
+                      width="68"
+                      height="74"
+                      viewBox="0 0 68 74"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M22.4259 68.5278C16.0259 66.7318 9.32534 65.8258 2.82534 64.9958C1.42534 64.8218 0.125535 65.7928 0.0255346 67.1608C-0.174465 68.5298 0.826121 69.7818 2.12612 69.9557C8.42612 70.7548 14.9255 71.6097 21.0255 73.3387C22.3255 73.7137 23.7261 72.9418 24.1261 71.6138C24.5261 70.2868 23.7259 68.9038 22.4259 68.5278Z"
+                        fill="black"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M41.8251 43.0649C31.5251 32.5539 19.9251 23.3959 9.8251 12.6029C8.9251 11.5949 7.3251 11.5409 6.3251 12.4819C5.3251 13.4239 5.22549 15.0079 6.22549 16.0159C16.3255 26.8399 27.9255 36.0279 38.2255 46.5699C39.2255 47.5539 40.8251 47.5679 41.8251 46.5999C42.7251 45.6329 42.8251 44.0489 41.8251 43.0649Z"
+                        fill="black"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M61.1264 2.63576C61.4264 8.65176 61.7259 14.6678 62.0259 20.6848C62.0259 22.0628 63.2264 23.1268 64.6264 23.0598C66.0264 22.9918 67.0259 21.8188 67.0259 20.4398C66.7259 14.4138 66.4264 8.38876 66.1264 2.36376C66.0264 0.985757 64.8262 -0.0712432 63.4262 0.00375683C62.1262 0.0787568 61.0264 1.25876 61.1264 2.63576Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
                 </div>
               ))}
             </div>
